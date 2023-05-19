3a518381dbfefd03041411232966a7d01d46462c
adam.poteralowicz <adam.poteralowicz@siili.com>
2023-05-19 09:07:07 +0200

    Add jackson dependency to build.gradle file programmatically

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -1,33 +1,30 @@
 const fs = require("fs");
 
-function androidXUpgrade (ctx) {
+function addJacksonDependency (ctx) {
     if (!ctx.opts.platforms.includes('android'))
         return;
+    
+    const jackson = "com.fasterxml.jackson.core:jackson-databind";
+    const buildGradlePath = "./platforms/android/build.gradle";
+    let buildGradle = fs.readFileSync(buildGradlePath, "utf-8");
+    if (buildGradle) {
+        const isJacksonPresent = buildGradle.includes(implementationJackson);
+        if (isJacksonPresent === false) {
+            const implementationJackson = "implementation \"" + jackson + ":2.0.1\"";
+            const dependencies = "dependencies {";
+            if (buildGradle.includes(dependencies) === false) {
+                buildGradle += '\n' +  dependencies + '\n' + "}";
+            }
+            let dependenciesBlockStart = buildGradle.indexOf(dependencies);
+            let dependenciesBlock = buildGradle.substring(dependenciesBlockStart, dependenciesBlockStart + 1);
+            let updatedDependencies = dependenciesBlock + '\n' + implementationJackson;
 
-    const enableAndroidX = "android.useAndroidX=true";
-    const enableJetifier = "android.enableJetifier=true";
-    const gradlePropertiesPath = "./platforms/android/gradle.properties";
-
-    let gradleProperties = fs.readFileSync(gradlePropertiesPath, "utf8");
-
-    if (gradleProperties)
-    {
-        const isAndroidXEnabled = gradleProperties.includes(enableAndroidX);
-        const isJetifierEnabled = gradleProperties.includes(enableJetifier);
-
-        if (isAndroidXEnabled && isJetifierEnabled)
-            return;
-
-        if (isAndroidXEnabled === false)
-            gradleProperties += "\n" + enableAndroidX;
-
-        if (isJetifierEnabled === false)
-            gradleProperties += "\n" + enableJetifier;
-
-        fs.writeFileSync(gradlePropertiesPath, gradleProperties);
+            buildGradle.replace(dependenciesBlock, updatedDependencies);
+            fs.writeFileSync(buildGradlePath, buildGradle);
+        }
     }
 }
 
 module.exports = function (ctx) {
-    androidXUpgrade(ctx);
+    addJacksonDependency(ctx);
 };
